5952b355064eaa8c95ff490e7a66446576f34c6e
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-04-06 16:04:55 -0300

    Adicionar toString na classe Orc

--- src/modulo-01-java-OO/Dia01/Orc.java
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -31,4 +31,16 @@ public class Orc
     public int getVida() {
         return this.vida;
     }
+    /**
+     * Imprime a vida atual do Orc
+     * 
+     * @return String com a vida atual do Orc. Ex: Vida Atual: 110
+     */
+    public String toString(){
+        return ("Vida Atual: " + this.vida);
+    }
+    //public String toString(){
+    //    return "oi";
+    //}
+    
 }
