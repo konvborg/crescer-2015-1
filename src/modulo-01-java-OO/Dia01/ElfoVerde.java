79186aa35b4929e54e3562cc7b69c7c9d5adda40
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-04-10 18:45:18 -0300

    Adicionar método AtirarFlecha e AdicionarItem para classe ElfoVerde.

--- src/modulo-01-java-OO/Dia01/ElfoVerde.java
+++ src/modulo-01-java-OO/Dia01/ElfoVerde.java
@@ -10,15 +10,21 @@ public class ElfoVerde extends Elfo
 public ElfoVerde(String nome){
     super(nome,0);
     }
+/**
+ * Chama o método atirar flecha, mas ganha mais um de experiencia.
+ * @param umOrc Orc que vai receber a flecha.
+ */
 protected void atirarFlecha(Orc umOrc) {
     //flechas = flechas - 1;
-    flechas--;
+    super.atirarFlecha(umOrc);
     experiencia++;
-    experiencia++;
-    umOrc.recebeAtaque();
     }
-protected void adicionarItem(ItemDoInventario item){
-    if((item.getDescricao()=="Espada de aço valiriano")||(item.getDescricao()=="Espada de aço valiriano")){
+/**
+ * Adiciona item no inventario desde que ele seja uma Espada de aço valiriano ou um Arco e Flecha de Vidro.
+ * @param ItemDoInventario item a ser adicionado
+ */
+    protected void adicionarItem(ItemDoInventario item){
+    if((item.getDescricao()=="Espada de aço valiriano")||(item.getDescricao()=="Arco e Flecha de Vidro")){
             this.itens.add(item);
     }else{
         return;
