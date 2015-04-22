163c3a717dc819b312a324f6142151068263726a
Mauricio Bassanesi Borges <mauricio.borges@cwi.com.br>
2015-04-22 15:01:20 -0300

    Atualizar com ex 6~10

--- src/modulo-02-sql/Exercicios/20-04-2015ex2.sql
+++ src/modulo-02-sql/Exercicios/20-04-2015ex2.sql
@@ -8,6 +8,7 @@ from Associado
 
 --3
 select NomeEmpregado as Nome,
+	   DataAdmissao,
 	   DateDiff(MONTH, DataAdmissao, convert(datetime, '31/12/2000', 103)) as Meses_trabalhados_ate_data
 from   Empregado
 where DataAdmissao Between convert(datetime, '01/05/1980', 103) and convert(datetime, '20/01/1982', 103);
@@ -29,3 +30,29 @@ Select nome,
 	   DATEADD(YEAR,50,DataNascimento) as Data_de_50_anos,
 	   datename(dw,DATEADD(YEAR,50,DataNascimento)) as Dia_Da_Semana
 from associado;
+
+--7
+Select UF, 
+	   count(1) as NumeroCidades
+From Cidade
+Group By UF;
+
+--8
+Select Nome,
+	   UF
+From Cidade
+Group by Nome, UF
+Having count(nome)>1 and count(UF)>1
+
+--9
+Select TOP 1 IDAssociado + 1 as proximo_id
+From Associado
+Order by IDAssociado desc
+
+--10
+WITH CTE AS(
+   SELECT nome, uf,
+       RN = ROW_NUMBER()OVER(PARTITION BY nome, uf ORDER BY nome, uf)
+   FROM CidadeAux
+)
+DELETE FROM CTE WHERE RN > 1	
