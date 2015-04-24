0123745778e79f774efd3acae9b7674090980609
Mauricio Bassanesi Borges <mauricio.borges@cwi.com.br>
2015-04-24 13:50:27 -0300

    Exercicios 5~8 do dia 22/04

--- src/modulo-02-sql/Exercicios/22-04-2015.sql
+++ src/modulo-02-sql/Exercicios/22-04-2015.sql
@@ -28,3 +28,49 @@ Left join (Select IDCidade, ('***') as Nome
 			From Cidade
 			Where UF in ('SC', 'PR', 'RS') )as city
 on city.IDCidade = a.IDCidade
+
+--5
+Select e.NomeEmpregado,
+	   g.NomeEmpregado as NomeGerente
+	   from Empregado e
+		 LEFT JOIN Empregado g
+		   ON e.IDGerente = g.IDEmpregado
+
+Select  e.NomeEmpregado,
+		d.NomeDepartamento
+from Empregado e
+	left join Departamento d
+		on e.IDDepartamento = d.IDDepartamento
+
+select * from empregado
+
+--6
+select *
+into EmpregadoAux
+from Empregado;
+
+begin transaction
+
+Update Empregado 
+set Salario = (Salario*1.145)
+Where IDDepartamento IN(Select IDDepartamento
+						From Departamento
+						Where Localizacao = 'SAO PAULO'
+						)
+commit
+
+--7
+Select SUM(ISNULL(e.Salario,0))-SUM(ISNULL(c.Salario,0)) as DiferencaPosAumento
+from Empregado e 
+	left join EmpregadoAux c on e.IDEmpregado = c.IDEmpregado
+
+--8
+Select NomeDepartamento
+from Departamento
+Where IDDepartamento = (Select TOP 1 d.IDDepartamento
+						from Empregado e 
+							inner join Departamento d ON d.IDDepartamento = e.IDDepartamento
+						order by Salario desc
+						)
+
+--9
