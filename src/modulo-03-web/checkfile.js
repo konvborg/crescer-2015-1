4e5f8059fe5c2c139d51d7e76d5d244e8ca85935
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-05-06 15:05:46 -0300

    Adicionar permissão para arquivos jpeg.

--- src/modulo-03-web/checkfile.js
+++ src/modulo-03-web/checkfile.js
@@ -4,11 +4,11 @@ function checkFile() {
     if (fileElement.value.lastIndexOf(".") > 0) {
         fileExtension = fileElement.value.substring(fileElement.value.lastIndexOf(".") + 1, fileElement.value.length);
     }
-    if (fileExtension == "png" || fileExtension == "jpg") {
+    if (fileExtension == "png" || fileExtension == "jpg" || fileExtension =="jpeg") {
         return;
     }
     else {
-        alert("A imagem deve ser .png ou .jpg");
+        alert("A imagem deve ser '.png.','.jpeg' ou '.jpg'");
         fileElement.value='';
         return;
     }
