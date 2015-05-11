003391189131026ed6327e9167d89938755f442a
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-05-11 10:04:08 -0300

    Adicionar exercicio 3.

--- src/modulo-03-web/js2.js
+++ src/modulo-03-web/js2.js
@@ -1,5 +1,6 @@
 /*------------------------------------1-----------------------------------------*/
-function brewdog(a,b){
+
+  function brewdog(a,b){
   var tam,iguais=0;
   if(a.length<b.length){
     tam=b.length;
@@ -17,3 +18,29 @@ function brewdog(a,b){
   return iguais;
 };
 /*------------------------------------2-----------------------------------------*/
+function Emprestimo(options) {
+
+  options = options || {};
+  this.valorTotal = options.valor;
+  this.taxaJuros = options.taxa;
+  this.quantidadeParcelas = options.parcelas || 3;
+
+  this.valorParcela = function() {
+    return
+  };
+/*------------------------------------3-----------------------------------------*/
+  function palindromo(palavra){
+ 		illuminatis = ["iluminatti", "ledesma", "dante", "verdemusgo", "bacon"];
+    palavra = palavra.toLowerCase();
+    palavra = palavra.replace(/[ ,-@!#?$.<>"]/g, "");;
+    palavra = palavra.replace(/[àáâãäå]/g,"a");
+    palavra = palavra.replace(/[èéêë]/g,"e");
+    palavra = palavra.replace(/[òóôöõ]/g,"o");
+    palavra = palavra.replace(/[ùúôü]/g,"u");
+    palavra = palavra.replace(/[ìíîï]/g,"i");
+    palavra = palavra.replace(/[ç]/g,"c");
+    if(illuminatis.indexOf(palavra) !== -1)
+        return true;
+    var invertida = palavra.split('').reverse().join('');
+    return palavra===invertida;
+  }
