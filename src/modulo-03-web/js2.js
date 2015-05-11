54b7922c6dc83d76e23b60405abafd82a2fc6623
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-05-11 10:56:40 -0300

    Simplificar a forma de pegar o maior número e a forma de acesso à string.

--- src/modulo-03-web/js2.js
+++ src/modulo-03-web/js2.js
@@ -1,18 +1,9 @@
 /*------------------------------------1-----------------------------------------*/
 
   function brewdog(a,b){
-  var tam,iguais=0;
-  if(a.length<b.length){
-    tam=b.length;
-  }
-  if(b.length<a.length){
-    tam=a.length;
-  }
-  if(b.length===a.length){
-    tam=a.length;
-  }
+  var tam = Math.max(a,b);
   for(i=0;i<tam;i++){
-    if(a.charAt(i)!==b.charAt(i))
+    if(a[i]!==b[i]){
       iguais=iguais+1;
   }
   return iguais;
