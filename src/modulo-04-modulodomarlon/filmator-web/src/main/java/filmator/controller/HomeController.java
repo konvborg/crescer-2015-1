2493ff28abdae0afbe457652e2ec030dc8238b88
Maurício Bassanesi Borges <mauricioborges_@live.com>
2015-05-27 20:21:36 -0300

    Corrigir bug onde usuários normais podiam acessar qualquer página.

--- src/modulo-04-modulodomarlon/filmator-web/src/main/java/filmator/controller/HomeController.java
+++ src/modulo-04-modulodomarlon/filmator-web/src/main/java/filmator/controller/HomeController.java
@@ -1,29 +1,21 @@
 package filmator.controller;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import filmator.dao.UsuarioDao;
 import filmator.model.TipoUsuario;
 import filmator.model.Usuario;
 
 @Controller
 public class HomeController {
-
-	
-	@Inject
-	private UsuarioDao dao;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Model model, HttpSession session) {
 		Usuario usuariologado = (Usuario) session.getAttribute("usuariologado");
 		if(usuariologado!=null){
-			boolean isadmin = usuariologado!=null||usuariologado.getTipo()!=TipoUsuario.ADMIN;
+			boolean isadmin = usuariologado.getTipo()==TipoUsuario.ADMIN;
 			model.addAttribute("isadmin", isadmin);
 			model.addAttribute("nome",usuariologado.getNome());
 			return "home";
